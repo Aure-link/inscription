59c3fd5d1733913d17bc57d3b12786d65ee9c140
aurelien <guillietaurelien@gmail.com>
2018-09-14 09:00:59 +0200

    travail de la semaine

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,30 @@
 (function($) {
-    $(document).ready(function(){
-        $('.form-field').click(function(){
-            $('.form-field label').removeClass('toTop');
-            $(this).children('label').addClass('toTop');
-        });
-        $('.form-field input').focus(function(){
-            $('.form-field label').removeClass('toTop');
+    $(document).ready(function() {
+        $('.form-field input').focus(function() {
             $(this).prev('label').addClass('toTop');
+        });
+        $('.form-field input').blur(function() {
+            if ($(this).val() == '') {
+                $(this).prev('label').removeClass('toTop');
+            }
+        });
+        $('.form-field.condition label').click(function() {
+            $(this).prev('.checkbox').toggleClass('checked');
+        });
+        $('#visible').click(function(){
+            $('.visible i').toggleClass('ion-eye-disabled');
+        	if($(this).is(':checked')) {
+            $('#password').get(0).type = 'text';
+          } else {
+              $('#password').get(0).type = 'password';
+          }
+        });
+        // NOTE: Password field
+        $('.form-field #password').focus(function(){
+            $('.info-password').slideToggle();
+        });
+        $('.form-field #password').blur(function() {
+            $('.info-password').slideToggle();
         });
     });
 })(jQuery);
